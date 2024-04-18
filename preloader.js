de34c23408bef6b964e6d0135bdde7d96086d786
Masane777 <47014083+Masane777@users.noreply.github.com>
2024-04-18 12:06:10 -0300

    Update preloader.js

--- preloader.js
+++ preloader.js
@@ -16,7 +16,7 @@ overlay.style.cssText = `
 
 const svgImage = document.createElement('img');
 svgImage.id = 'svgImage';
-svgImage.src = 'https://masane777.github.io/preloaderJs/imagem.svg';
+svgImage.src = 'https://masane777.github.io/preloaderJs/PreloaderGallixQuadrados.svg';
 svgImage.style.cssText = `
     max-width: 100%;
     max-height: 100%;
